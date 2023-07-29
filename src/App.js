5fcd5319a1c2f218fe2836de602f8163e88b0565
arunmannai <arunmannai@gmail.com>
2023-07-29 23:05:03 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Header from './Header';
+import Main from './Main';
 
-export default function App() {
+function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Header />
+      <Main />
     </div>
   );
 }
+
+export default App;
