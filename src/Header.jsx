ff9556cab1c82de6f4531f2867b1612f71c50821
Arun Baskaran <arunmannai@gmail.com>
2024-03-01 14:10:22 +0530

    ISP Directory

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import Counter from './Counter';
 function Header(props) {
   return (
     <div className="headercompo">
-      <h1 className="heading">ISP HackerEarth</h1>
+      <h1 className="heading">ISP Directory</h1>
       <span className="subheading">- searching ISP made easy!</span>
       <Counter total={props.total} apihits={props.apihits} />
     </div>
